55d37413a1202bd3af3eb41e83efeee5320764e9
enzosantanaf <enzosantanaf@outlook.com>
2022-06-21 22:33:05 -0300

    código js refatorado

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//variável master
 let data = new Date()
 /*
 Days
@@ -9,6 +10,10 @@ Days
 5 = Sexta
 6 = Sábado
 */
+
+//variáveis globais
+let dia = data.getDay()
+let hora = data.getHours()
 const bgHeader = document.querySelector('header')
 const msgSaudacao = document.querySelector('h1')
 const msgSaudacaoSub = document.querySelector('h3')
@@ -19,134 +24,157 @@ const horaAula = document.querySelector('ul div span.hora-aula')
 const livroUsado = document.querySelector('ul .horario .livro')
 
 function saudacao() {
-  hora = data.getHours()
   if (hora >= 0 && hora < 12) {
     msgSaudacao.innerHTML = 'Bom Dia!'
   } else if (hora >= 12 && hora < 18) {
     msgSaudacao.innerHTML = 'Boa Tarde!'
+  } else if (hora >= 18) {
+    msgSaudacao.innerHTML = 'Boa Noite!'
+  }
+}
+
+function saudacaoModLayout() {
+  if (hora >= 0 && hora < 12) {
+  } else if (hora >= 12 && hora < 18) {
     msgSaudacao.style.color = 'var(--cor-6)'
     msgSaudacaoSub.style.color = 'var(--cor-2)'
     bgHeader.style.backgroundColor = 'var(--cor-4)'
   } else if (hora >= 18) {
-    msgSaudacao.innerHTML = 'Boa Noite!'
     msgSaudacao.style.color = 'var(--cor-6)'
     msgSaudacaoSub.style.color = 'var(--cor-3)'
     bgHeader.style.backgroundColor = 'var(--cor-8)'
   }
 }
 
-function aulaELivroEmAndamento() {
-  dia = data.getDay()
-  hora = data.getHours()
-  if (dia == 1 && hora >= 13 && hora < 14) {
+function ordemAula() {
+  if (hora >= 13 && hora < 14) {
     horaAula.innerHTML = '1° | '
-    aula.innerHTML = aulas.segunda.primeiro
-    livroUsado.innerHTML = '-'
-  } else if (dia == 1 && hora >= 14 && hora < 15) {
+  } else if (hora >= 14 && hora < 15) {
     horaAula.innerHTML = '2° | '
-    aula.innerHTML = aulas.segunda.segundo
-    livroUsado.innerHTML = livro[3]
-  } else if (dia == 1 && hora >= 15 && hora < 16) {
+  } else if (hora >= 15 && hora < 16) {
     horaAula.innerHTML = '3° | '
-    aula.innerHTML = aulas.segunda.terceiro
-    livroUsado.innerHTML = livro[1]
-  } else if (dia == 1 && hora >= 16 && hora < 17) {
+  } else if (hora >= 16 && hora < 17) {
     horaAula.innerHTML = '4° | '
-    aula.innerHTML = aulas.segunda.quarto
-    livroUsado.innerHTML = livro[3]
-  } else if (dia == 1 && hora >= 17 && hora < 18) {
+  } else if (hora >= 17 && hora < 18) {
     horaAula.innerHTML = '5° | '
-    aula.innerHTML = aulas.segunda.quinto
-    livroUsado.innerHTML = livro[2]
-  } else if (dia == 2 && hora >= 13 && hora < 14) {
-    horaAula.innerHTML = '1° | '
-    aula.innerHTML = aulas.terca.primeiro
-    livroUsado.innerHTML = livro[4]
-  } else if (dia == 2 && hora >= 14 && hora < 15) {
-    horaAula.innerHTML = '2° | '
-    aula.innerHTML = aulas.terca.segundo
-    livroUsado.innerHTML = '-'
-  } else if (dia == 2 && hora >= 15 && hora < 16) {
-    horaAula.innerHTML = '3° | '
-    aula.innerHTML = aulas.terca.terceiro
-    livroUsado.innerHTML = '-'
-  } else if (dia == 2 && hora >= 16 && hora < 17) {
-    horaAula.innerHTML = '4° | '
-    aula.innerHTML = aulas.terca.quarto
-    livroUsado.innerHTML = '-'
-  } else if (dia == 2 && hora >= 17 && hora < 18) {
-    horaAula.innerHTML = '5° | '
-    aula.innerHTML = aulas.terca.quinto
-    livroUsado.innerHTML = '-'
-  } else if (dia == 3 && hora >= 13 && hora < 14) {
-    horaAula.innerHTML = '1° | '
-    aula.innerHTML = aulas.quarta.primeiro
-    livroUsado.innerHTML = livro[2]
-  } else if (dia == 3 && hora >= 14 && hora < 15) {
-    horaAula.innerHTML = '2° | '
-    aula.innerHTML = aulas.quarta.segundo
-    livroUsado.innerHTML = livro[1]
-  } else if (dia == 3 && hora >= 15 && hora < 16) {
-    horaAula.innerHTML = '3° | '
-    aula.innerHTML = aulas.quarta.terceiro
-    livroUsado.innerHTML = livro[1]
-  } else if (dia == 3 && hora >= 16 && hora < 17) {
-    horaAula.innerHTML = '4° | '
-    aula.innerHTML = aulas.quarta.quarto
-    livroUsado.innerHTML = livro[2]
-  } else if (dia == 3 && hora >= 17 && hora < 18) {
-    horaAula.innerHTML = '5° | '
-    aula.innerHTML = aulas.quarta.quinto
-    livroUsado.innerHTML = livro[1]
-  } else if (dia == 4 && hora >= 13 && hora < 14) {
-    horaAula.innerHTML = '1° | '
-    aula.innerHTML = aulas.quinta.primeiro
-    livroUsado.innerHTML = livro[1]
-  } else if (dia == 4 && hora >= 14 && hora < 15) {
-    horaAula.innerHTML = '2° | '
-    aula.innerHTML = aulas.quinta.segundo
-    livroUsado.innerHTML = livro[4]
-  } else if (dia == 4 && hora >= 15 && hora < 16) {
-    horaAula.innerHTML = '3° | '
-    aula.innerHTML = aulas.quinta.terceiro
-    livroUsado.innerHTML = livro[4]
-  } else if (dia == 4 && hora >= 16 && hora < 17) {
-    horaAula.innerHTML = '4° | '
-    aula.innerHTML = aulas.quinta.quarto
-    livroUsado.innerHTML = livro[1]
-  } else if (dia == 4 && hora >= 17 && hora < 18) {
-    horaAula.innerHTML = '5° | '
-    aula.innerHTML = aulas.quinta.quinto
-    livroUsado.innerHTML = livro[1]
-  } else if (dia == 5 && hora >= 13 && hora < 14) {
-    horaAula.innerHTML = '1° | '
-    aula.innerHTML = aulas.sexta.primeiro
-    livroUsado.innerHTML = '-'
-  } else if (dia == 5 && hora >= 14 && hora < 15) {
-    horaAula.innerHTML = '2° | '
-    aula.innerHTML = aulas.sexta.segundo
-    livroUsado.innerHTML = livro[1]
-  } else if (dia == 5 && hora >= 15 && hora < 16) {
-    horaAula.innerHTML = '3° | '
-    aula.innerHTML = aulas.sexta.terceiro
-    livroUsado.innerHTML = '-'
-  } else if (dia == 5 && hora >= 16 && hora < 17) {
-    horaAula.innerHTML = '4° | '
-    aula.innerHTML = aulas.sexta.quarto
-    livroUsado.innerHTML = '-'
-  } else if (dia == 5 && hora >= 17 && hora < 18) {
-    horaAula.innerHTML = '5° | '
-    aula.innerHTML = aulas.sexta.quinto
-    livroUsado.innerHTML = '-'
   } else {
     horaAula.style.display = 'none'
+  }
+}
+
+function aulaEmAndamento() {
+  if (dia == 1 && hora >= 13 && hora < 14) {
+    aula.innerHTML = aulas.segunda.primeiro
+  } else if (dia == 1 && hora >= 14 && hora < 15) {
+    aula.innerHTML = aulas.segunda.segundo
+  } else if (dia == 1 && hora >= 15 && hora < 16) {
+    aula.innerHTML = aulas.segunda.terceiro
+  } else if (dia == 1 && hora >= 16 && hora < 17) {
+    aula.innerHTML = aulas.segunda.quarto
+  } else if (dia == 1 && hora >= 17 && hora < 18) {
+    aula.innerHTML = aulas.segunda.quinto
+  } else if (dia == 2 && hora >= 13 && hora < 14) {
+    aula.innerHTML = aulas.terca.primeiro
+  } else if (dia == 2 && hora >= 14 && hora < 15) {
+    aula.innerHTML = aulas.terca.segundo
+  } else if (dia == 2 && hora >= 15 && hora < 16) {
+    aula.innerHTML = aulas.terca.terceiro
+  } else if (dia == 2 && hora >= 16 && hora < 17) {
+    aula.innerHTML = aulas.terca.quarto
+  } else if (dia == 2 && hora >= 17 && hora < 18) {
+    aula.innerHTML = aulas.terca.quinto
+  } else if (dia == 3 && hora >= 13 && hora < 14) {
+    aula.innerHTML = aulas.quarta.primeiro
+  } else if (dia == 3 && hora >= 14 && hora < 15) {
+    aula.innerHTML = aulas.quarta.segundo
+  } else if (dia == 3 && hora >= 15 && hora < 16) {
+    aula.innerHTML = aulas.quarta.terceiro
+  } else if (dia == 3 && hora >= 16 && hora < 17) {
+    aula.innerHTML = aulas.quarta.quarto
+  } else if (dia == 3 && hora >= 17 && hora < 18) {
+    aula.innerHTML = aulas.quarta.quinto
+  } else if (dia == 4 && hora >= 13 && hora < 14) {
+    aula.innerHTML = aulas.quinta.primeiro
+  } else if (dia == 4 && hora >= 14 && hora < 15) {
+    aula.innerHTML = aulas.quinta.segundo
+  } else if (dia == 4 && hora >= 15 && hora < 16) {
+    aula.innerHTML = aulas.quinta.terceiro
+  } else if (dia == 4 && hora >= 16 && hora < 17) {
+    aula.innerHTML = aulas.quinta.quarto
+  } else if (dia == 4 && hora >= 17 && hora < 18) {
+    aula.innerHTML = aulas.quinta.quinto
+  } else if (dia == 5 && hora >= 13 && hora < 14) {
+    aula.innerHTML = aulas.sexta.primeiro
+  } else if (dia == 5 && hora >= 14 && hora < 15) {
+    aula.innerHTML = aulas.sexta.segundo
+  } else if (dia == 5 && hora >= 15 && hora < 16) {
+    aula.innerHTML = aulas.sexta.terceiro
+  } else if (dia == 5 && hora >= 16 && hora < 17) {
+    aula.innerHTML = aulas.sexta.quarto
+  } else if (dia == 5 && hora >= 17 && hora < 18) {
+    aula.innerHTML = aulas.sexta.quinto
+  } else {
     aula.innerHTML = '-'
+  }
+}
+
+function livroSendoUsado() {
+  if (dia == 1 && hora >= 13 && hora < 14) {
+    livroUsado.innerHTML = '-'
+  } else if (dia == 1 && hora >= 14 && hora < 15) {
+    livroUsado.innerHTML = livro[3]
+  } else if (dia == 1 && hora >= 15 && hora < 16) {
+    livroUsado.innerHTML = livro[1]
+  } else if (dia == 1 && hora >= 16 && hora < 17) {
+    livroUsado.innerHTML = livro[3]
+  } else if (dia == 1 && hora >= 17 && hora < 18) {
+    livroUsado.innerHTML = livro[2]
+  } else if (dia == 2 && hora >= 13 && hora < 14) {
+    livroUsado.innerHTML = livro[4]
+  } else if (dia == 2 && hora >= 14 && hora < 15) {
+    livroUsado.innerHTML = '-'
+  } else if (dia == 2 && hora >= 15 && hora < 16) {
+    livroUsado.innerHTML = '-'
+  } else if (dia == 2 && hora >= 16 && hora < 17) {
+    livroUsado.innerHTML = '-'
+  } else if (dia == 2 && hora >= 17 && hora < 18) {
+    livroUsado.innerHTML = '-'
+  } else if (dia == 3 && hora >= 13 && hora < 14) {
+    livroUsado.innerHTML = livro[2]
+  } else if (dia == 3 && hora >= 14 && hora < 15) {
+    livroUsado.innerHTML = livro[1]
+  } else if (dia == 3 && hora >= 15 && hora < 16) {
+    livroUsado.innerHTML = livro[1]
+  } else if (dia == 3 && hora >= 16 && hora < 17) {
+    livroUsado.innerHTML = livro[2]
+  } else if (dia == 3 && hora >= 17 && hora < 18) {
+    livroUsado.innerHTML = livro[1]
+  } else if (dia == 4 && hora >= 13 && hora < 14) {
+    livroUsado.innerHTML = livro[1]
+  } else if (dia == 4 && hora >= 14 && hora < 15) {
+    livroUsado.innerHTML = livro[4]
+  } else if (dia == 4 && hora >= 15 && hora < 16) {
+    livroUsado.innerHTML = livro[4]
+  } else if (dia == 4 && hora >= 16 && hora < 17) {
+    livroUsado.innerHTML = livro[1]
+  } else if (dia == 4 && hora >= 17 && hora < 18) {
+    livroUsado.innerHTML = livro[1]
+  } else if (dia == 5 && hora >= 13 && hora < 14) {
+    livroUsado.innerHTML = '-'
+  } else if (dia == 5 && hora >= 14 && hora < 15) {
+    livroUsado.innerHTML = livro[1]
+  } else if (dia == 5 && hora >= 15 && hora < 16) {
+    livroUsado.innerHTML = '-'
+  } else if (dia == 5 && hora >= 16 && hora < 17) {
+    livroUsado.innerHTML = '-'
+  } else if (dia == 5 && hora >= 17 && hora < 18) {
+    livroUsado.innerHTML = '-'
+  } else {
     livroUsado.innerHTML = '-'
   }
 }
 
-function horario(dia) {
-  dia = data.getDay()
+function diaDeHoje() {
   switch (dia) {
     case 1:
       horarioDia.innerHTML = 'SEG'
@@ -166,21 +194,49 @@ function horario(dia) {
     default:
       horarioDia.style.display = 'none'
   }
+}
+
+function horario() {
   switch (dia) {
     case 1:
-      horarioCompleto.innerHTML = `1° | ${aulas.segunda.primeiro}</br>2° | ${aulas.segunda.segundo}</br>3° | ${aulas.segunda.terceiro}</br>4° | ${aulas.segunda.quarto}</br>5° | ${aulas.segunda.quinto}`
+      horarioCompleto.innerHTML = `
+      1° | ${aulas.segunda.primeiro}</br>
+      2° | ${aulas.segunda.segundo}</br>
+      3° | ${aulas.segunda.terceiro}</br>
+      4° | ${aulas.segunda.quarto}</br>
+      5° | ${aulas.segunda.quinto}`
       break
     case 2:
-      horarioCompleto.innerHTML = `1° | ${aulas.terca.primeiro}</br>2° | ${aulas.terca.segundo}</br>3° | ${aulas.terca.terceiro}</br>4° | ${aulas.terca.quarto}</br>5° | ${aulas.terca.quinto}`
+      horarioCompleto.innerHTML = `
+      1° | ${aulas.terca.primeiro}</br>
+      2° | ${aulas.terca.segundo}</br>
+      3° | ${aulas.terca.terceiro}</br>
+      4° | ${aulas.terca.quarto}</br>
+      5° | ${aulas.terca.quinto}`
       break
     case 3:
-      horarioCompleto.innerHTML = `1° | ${aulas.quarta.primeiro}</br>2° | ${aulas.quarta.segundo}</br>3° | ${aulas.quarta.terceiro}</br>4° | ${aulas.quarta.quarto}</br>5° | ${aulas.quarta.quinto}`
+      horarioCompleto.innerHTML = `
+      1° | ${aulas.quarta.primeiro}</br>
+      2° | ${aulas.quarta.segundo}</br>
+      3° | ${aulas.quarta.terceiro}</br>
+      4° | ${aulas.quarta.quarto}</br>
+      5° | ${aulas.quarta.quinto}`
       break
     case 4:
-      horarioCompleto.innerHTML = `1° | ${aulas.quinta.primeiro}</br>2° | ${aulas.quinta.segundo}</br>3° | ${aulas.quinta.terceiro}</br>4° | ${aulas.quinta.quarto}</br>5° | ${aulas.quinta.quinto}`
+      horarioCompleto.innerHTML = `
+      1° | ${aulas.quinta.primeiro}</br>
+      2° | ${aulas.quinta.segundo}</br>
+      3° | ${aulas.quinta.terceiro}</br>
+      4° | ${aulas.quinta.quarto}</br>
+      5° | ${aulas.quinta.quinto}`
       break
     case 5:
-      horarioCompleto.innerHTML = `1° | ${aulas.sexta.primeiro}</br>2° | ${aulas.sexta.segundo}</br>3° | ${aulas.sexta.terceiro}</br>4° | ${aulas.sexta.quarto}</br>5° | ${aulas.sexta.quinto}`
+      horarioCompleto.innerHTML = `
+      1° | ${aulas.sexta.primeiro}</br>
+      2° | ${aulas.sexta.segundo}</br>
+      3° | ${aulas.sexta.terceiro}</br>
+      4° | ${aulas.sexta.quarto}</br>
+      5° | ${aulas.sexta.quinto}`
       break
     default:
       const horarioCompletoPai = document.querySelector('div.horario-dia')
@@ -191,5 +247,9 @@ function horario(dia) {
 }
 
 saudacao()
-aulaELivroEmAndamento()
+saudacaoModLayout()
+ordemAula()
+aulaEmAndamento()
+livroSendoUsado()
+diaDeHoje()
 horario()
